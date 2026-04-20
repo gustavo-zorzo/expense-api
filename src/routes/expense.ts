ffe6ad5e35280be7e7f3d16b86ae2ad0e613412e
Gustavo Zorzo <gustavo.zorzo@v4company.com>
2026-04-20 15:11:24 -0300

    feat: add GET route for fetching a specific expense by ID

--- src/routes/expense.ts
+++ src/routes/expense.ts
@@ -1,6 +1,7 @@
 import type { FastifyInstance } from 'fastify';
 import { prisma } from '../lib/prisma.js';
-import { createExpenseSchema } from '../schemas/expense.js';
+import { createExpenseSchema, idParamSchema } from '../schemas/expense.js';
+
 export function expenseRoutes(fastify: FastifyInstance) {
   fastify.get('/expenses', async (request, reply) => {
     return await reply
@@ -12,6 +13,22 @@ export function expenseRoutes(fastify: FastifyInstance) {
       const body = createExpenseSchema.parse(request.body);
       await prisma.expense.create({ data: body });
       reply.status(201).send({ status: 'Successfully created' });
+    } catch (e) {
+      if (e instanceof Error) {
+        reply.status(400).send({ error: e.message, status: 'Error Ocurred' });
+      }
+    }
+  });
+  fastify.get('/expenses/:id', async (request, reply) => {
+    try {
+      const id = idParamSchema.parse(request.params);
+      const expense = await prisma.expense.findUnique({ where: id });
+      if (expense === null) {
+        return reply
+          .status(404)
+          .send({ error: 'Id not found', status: 'Error Ocurred' });
+      }
+      reply.status(200).send({ expense: expense });
     } catch (e) {
       if (e instanceof Error) {
         reply.status(400).send({ error: e.message, status: 'Error Ocurred' });
